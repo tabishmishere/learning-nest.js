466246e9cd4e6a6124e314d700de4db2facd3d80
Muhammad Tabish <tabishm484@gmail.com>
2026-06-07 14:45:57 +0500

    feat: add CRUD operations foradd and get Car module in controller and service.

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { CarService } from './car.service';
+
 @Controller('car')
 export class CarController {
   constructor(private carService: CarService) {}
@@ -7,5 +8,15 @@ export class CarController {
   @Get('all')
   async getAllCars() {
     return this.carService.getAllCars();
+  }
+
+  @Get(':id')
+  async getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carService.getCarById(id);
+  }
+
+  @Post('add')
+  async addCar(@Body() car: { make: string; model: string; year: number }) {
+    return this.carService.addCar(car);
   }
 }
--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CARS } from './cars.mock';
 
 @Injectable()
 export class CarService {
   private cars = CARS;
+
+  //   Get all cars
   public async getAllCars() {
     return this.cars;
+  }
+
+  //   Get a car by its ID
+  public async getCarById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id ${id} not found`);
+    }
+
+    return car;
+  }
+
+  // Add a new car to the list
+  public async addCar(car: { make: string; model: string; year: number }) {
+    const newCar = { id: this.cars.length + 1, ...car };
+    this.cars.push(newCar);
+    return { message: 'Car Saved Successfully' };
   }
 }
